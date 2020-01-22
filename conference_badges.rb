186645f7b9ea0de10740178efec04c0e5fcddc0b
Kelly Powers <kpowers.nj@gmail.com>
2020-01-22 01:19:02 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,8 +12,9 @@ end
 
 def assign_rooms(attendees)
   room = 1 
+  new_array = []
   attendees.map do |attendee|
-    "Hello, #{attendee}! You'll be assigned to room #{room}!"
+    new_array << "Hello, #{attendee}! You'll be assigned to room #{room}!"
     room += 1 
   end
 end
